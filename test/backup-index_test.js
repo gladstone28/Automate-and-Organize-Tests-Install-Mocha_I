7f65f9160a92208d25a3b494a4ff45d480a29dc5
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-08 20:15:05 -0500

    first commit

---
+++ test/backup-index_test.js
@@ -0,0 +1,11 @@
+
+const assert = require('assert');
+
+describe("Math", () => {
+  describe(".min", () => {
+    it("returns the argument with the lowest value", () => {});
+  });
+  it("returns -Infinity when no arguments are provided", () => {
+    // Your test goes here
+  });
+});
